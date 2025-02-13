febc05fd5b06db179642b7aefbc5f8b0ffe6fc1a
Jakub Vaněk <linuxtardis@gmail.com>
2025-02-13 12:01:14 +0100

    chore: Make KF demo script usable

--- Tram2DKF/scripts/estimation_test.jl
+++ Tram2DKF/scripts/estimation_test.jl
@@ -1,8 +1,17 @@
 using Tram2DKF
-using Tram2DKF: PolarCTRA
+using Tram2DKF: PolarCTRA, UncertainValue
 using LinearAlgebra
 import Tram2DKF.nstates, Tram2DKF.ninputs, Tram2DKF.noutputs
+using Plots
 
+extract_ts(array::Vector{Vector{Float64}}, index::Int) =
+    array .|> item -> item[index]
+
+extract_ts(array::Vector{<:UncertainValue}, index::Int) =
+    array .|> item -> mean(item)[index]
+
+extract_cov_ts(array::Vector{<:UncertainValue}, i::Int, j::Int) =
+    array .|> item -> covariance(item)[i,j]
 
 Ts = 0.1
 
@@ -27,6 +36,8 @@ trip = [
     Stop(duration=10.0),
 ]
 
+# trip = [ConstantSpeed(speed=5, distance=90)]
+
 states = render_trip(track, trip, Ts, 10)
 
 gyro_std = 0.02
@@ -39,7 +50,7 @@ acc  = simulate_accelerometer(states, acc_std)
 odo  = simulate_odometry(states, odo_res)
 gnss = simulate_gnss(states, 200, gnss_cov)
 
-odo_v = (odo[2:end] - odo[1:end-1]) / Ts
+odo_v = vcat([0], (odo[2:end] - odo[1:end-1]) / Ts)
 odo_std = (odo_res/Ts)/sqrt(12)
 
 ax = acc  .|> x -> x[1];
@@ -89,15 +100,15 @@ transition_model = discretize(transition_model_ct, RK4(), Ts, 1)
 
 process_noise = Gaussian(
     zeros(Float64, nstates(transition_model)),
-    diagm([0.0, 0.0, 0.0, 0.1, 0.0, 0.1])
+    diagm([1e-9, 1e-9, 1e-9, 0.1, 1e-9, 0.01])
 )
 
 current_hypothesis = Gaussian(
     zeros(Float64, nstates(transition_model)),
-    diagm([10, 10, 0.1, 0.1, 2*pi, 0.1])
+    diagm([10, 10, 0.01, 0.01, 2*pi, 0.01])
 )
 
-hypotheses = [current_hypothesis]
+hypotheses = Vector{Gaussian}()
 filter = ExtendedKalmanFilter()
 
 steps = length(states)
@@ -107,18 +118,24 @@ for i in 1:steps
     if mod(i, 10) == 1
         meas_model = GNSS_ODO_IMU
         meas_cov = diagm([gnss_std^2, gnss_std^2, odo_std^2, gyro_std^2])
-        meas_val = [gnss[i][1], gnss[i][2], odo[i], wz[i]]
+        meas_val = [gnss[i][1], gnss[i][2], odo_v[i], wz[i]]
     else
         meas_model = ODO_IMU
         meas_cov = diagm([odo_std^2, gyro_std^2])
-        meas_val = [odo[i], wz[i]]
+        meas_val = [odo_v[i], wz[i]]
     end
 
-    posterior = data_step(filter, meas_model, current_hypothesis, [], Gaussian(meas_val, meas_cov))
+    measurement = Gaussian(meas_val, meas_cov)
+    posterior = data_step(filter, meas_model, current_hypothesis, [], measurement)
     push!(hypotheses, posterior)
 
     current_hypothesis = forward_step(filter, transition_model, posterior, [], process_noise)
 end
 
-estx = hypotheses .|> h -> mean(h)[1]
-esty = hypotheses .|> h -> mean(h)[2]
+
+estx = extract_ts(hypotheses, 1)
+esty = extract_ts(hypotheses, 2)
+estv = extract_ts(hypotheses, 3)
+# plot(t, esty)
+plot(estx, esty; aspect_ratio=:equal)
+# plot(t, estv)
