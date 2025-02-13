98b78889aaeba270f62f26c5c9774ebbc6a5331e
Jakub Vaněk <linuxtardis@gmail.com>
2025-02-14 00:16:05 +0100

    chore: Add Documenter.jl docs template

---
+++ docs/make.jl
@@ -0,0 +1,23 @@
+using Tram2DKF
+using Documenter
+
+DocMeta.setdocmeta!(Tram2DKF, :DocTestSetup, :(using Tram2DKF); recursive=true)
+
+makedocs(;
+    modules=[Tram2DKF],
+    authors="Jakub Vaněk <vanekj19@fel.cvut.cz> and contributors",
+    sitename="Tram2DKF",
+    format=Documenter.HTML(;
+        canonical="https://JakubVanek.github.io/Tram2DKF",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/JakubVanek/Tram2DKF",
+    devbranch="main",
+)
---
+++ docs/src/index.md
@@ -0,0 +1,14 @@
+```@meta
+CurrentModule = Tram2DKF
+```
+
+# Tram2DKF
+
+Documentation for [Tram2DKF](https://github.com/JakubVanek/Tram2DKF).
+
+```@index
+```
+
+```@autodocs
+Modules = [Tram2DKF]
+```
